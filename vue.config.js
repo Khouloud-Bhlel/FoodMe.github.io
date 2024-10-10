f5779dea6c2e33f0eddbaf3e0be016b2f68874eb
khouloud ben hlel <khouloudbenhelal24@gmail.com>
2024-10-11 00:19:08 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/FoodMe.github.io/'  // Use your repository name here
-      : '/'
+      ? '/FoodMe.github.io/'
+      : '/',
+    outputDir: 'docs'  // This will output the build to a 'docs' folder
   }
-  
